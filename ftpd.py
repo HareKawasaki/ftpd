68c97b0961911bf54bf91c6a569be5cd0a96cf75
HareKawasaki <laiyiduo@qq.com>
2025-05-25 21:16:52 +0800

    Debug

--- ftpd.py
+++ ftpd.py
@@ -2,11 +2,22 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from threading import Timer,Thread
+import os
 def getmessage():
     print('Use this script will share files without authentication')
     address=input('File Address:')
-    port=int(input('Port:'))
-    stop_time=float(input('How many minutes do you want the program to stop:'))*60
+    if address=='':        
+        address=os.getcwd()
+    port=input('Port:')
+    if port=='':
+        port=2121
+    else:
+        port=int(port)
+    stop_time=input('How many minutes do you want the program to stop:')
+    if stop_time=='':
+        stop_time=0
+    else:
+        stop_time=float(stop_time)*60
     return (address,port,stop_time)
 def create_server(address,port,stop):
     users=DummyAuthorizer()
